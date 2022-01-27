e66488f3fd5c43d966e18bc57f000e1f5f409ae5
Aleksandr <it.kucherov@gmail.com>
2022-01-27 20:22:51 +0300

    work number TWO

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+//let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
 const personalMovieDb = {
-	count: numberOfFilms,
+	//count: numberOfFilms,
 	movies: {},
 	actors: {},
 	genres: [],
 	privat: false,
 };
 
-const a = prompt('Назовите последний один из просмотренных фильмов?', '');
-const b = prompt('На сколько вы оцените его?', '');
-const c = prompt('Назовите последний один из просмотренных фильмов?', '');
-const d = prompt('На сколько вы оцените его?', '');
-//personalMovieDb.movies['lastFilms'];
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
+//if (numberOfFilms < 10) {
+//	alert('Вы посмотрели не так много фильмов');
+//} else if (numberOfFilms > 30) {
+//	alert('Вы киноман!');
+//} else if (numberOfFilms <= 30 || numberOfFilms >= 10) {
+//	alert('Вы классный зритель!');
+//} else {
+//	alert('Произошла ошибка');
+//}
+
+let a, b, yes;
+do {
+	do {
+		a = prompt('Назовите последний один из просмотренных фильмов?', '');
+	} while (a == '' || a.length > 3);
+	do {
+		b = prompt('На сколько вы оцените его?', '');
+	} while (b == '' || b.length > 4);
+
+	personalMovieDb.movies[a] = b;
+	yes = confirm('Qestion?');
+	if (!yes) {
+		break;
+	}
+} while (yes || a || b);
 
 console.log(personalMovieDb);
+
+//switch (numberOfFilms) {
+//	case numberOfFilms < 10:
+//		alert('Просмотрено довольно мало фильмов');
+//		break;
+//	case numberOfFilms > 30:
+//		alert('Вы киноман!');
+//		break;
+//	case numberOfFilms >= 10 || numberOfFilms <= 30:
+//		alert('Вы классный зритель!');
+//		break;
+//	default:
+//		alert('Произошла ошибка!');
+//}
